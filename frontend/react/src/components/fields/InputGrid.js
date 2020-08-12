89d8d079c572edbed5f53b83def34b4a6955c608
Claire Annan <claire.annan@gsa.gov>
2020-08-12 04:56:53 -0400

    Remove checkboxFlag. Show question title, marker, label and hint for each grouping.

--- frontend/react/src/components/fields/InputGrid.js
+++ frontend/react/src/components/fields/InputGrid.js
@@ -2,24 +2,21 @@ import React from "react"
 import { TextField } from "@cmsgov/design-system-core"
 import { Choice } from "@cmsgov/design-system-core"
 
-export const InputGrid = props => {
-  const index = props.index
-  const checkedTotal = <TextField name={`p3_q3_${index}_total`} numeric label="0-16 years old" size="small" />
+export const InputGrid = question => {
   return (
     <div className="input-grid ds-l-container ds-u-padding--0">
-      <h4 className="ds-u-margin-bottom--0 ds-u-padding-top--2">{index}. {props.title}</h4>
-      <div className="ds-l-row ds-u-margin-top--0">
-        <div className="ds-l-col"><TextField name={`p3_q3_${index}_a`} numeric label="0-1 year old" /></div>
-        <div className="ds-l-col"><TextField name={`p3_q3_${index}_b`} numeric label="1-5 years old" /></div>
-        <div className="ds-l-col"><TextField name={`p3_q3_${index}_c`} numeric label="6-12 years old" /></div>
-        <div className="ds-l-col"><TextField name={`p3_q3_${index}_d`} numeric label="13-16 years old" /></div>
+      <h4 className="ds-u-margin-bottom--0 ds-u-padding-top--2">{question.title}</h4>
+      <label className="ds-c-label" fieldId={`2020-03-c-04-${question.marker}`} >{`${question.marker}. ${question.label}`}</label>
+      <span className="ds-c-field__hint">{question.hint}</span>
+      <div className="ds-l-row">
+        <div className="ds-l-col"><TextField name={`2020-03-c-04-${question.marker}-a`} id={`${question.id}-a`} className="input-grid__total" numeric label="Total for all ages (0-16 year olds)" size="medium" /></div>
       </div>
-      <Choice
-        name={`p3_q3_${index}_total_check`}
-        checkedChildren={<div className="ds-c-choice__checkedChild">{checkedTotal}</div>}
-      >
-        You only have the total number of children between 0-16 years old</Choice>
+      <div className="ds-l-row ds-u-margin-top--0">
+        <div className="ds-l-col"><TextField name={`${question.id}-b`} id={`${question.id}-b`} numeric label="0-1 year olds" /></div>
+        <div className="ds-l-col"><TextField name={`${question.id}-c`} id={`${question.id}-c`} numeric label="1-5 year olds" /></div>
+        <div className="ds-l-col"><TextField name={`${question.id}-d`} id={`${question.id}-d`} numeric label="6-12 year olds" /></div>
+        <div className="ds-l-col"><TextField name={`${question.id}-e`} id={`${question.id}-e`} numeric label="13-16 year olds" /></div>
+      </div>
     </div>
   )
-
 }
